567b64c0c9d7ec5c0d55b2d6110dd0d364360d34
Meni Yakove <myakove@gmail.com>
2023-05-15 17:13:14 +0300

    Slack notification

--- app/app.py
+++ app/app.py
@@ -47,13 +47,14 @@ def get_api(url, token):
     return gitlab_api
 
 
-def process_hook(api, data):
+def process_hook(api, data, slack_webhook_url):
     object_attributes = data["object_attributes"]
     if object_attributes.get("action") == "merge":
         project = api.projects.get(data["project"]["id"])
         merge_request = project.mergerequests.get(object_attributes["iid"])
         for change in merge_request.changes().get("changes", []):
             changed_file = change.get("new_path")
+            # TODO: Get addon version from changed_file and send it to slack
             matches = re.match(
                 r"addons/(?P<addon>.*)/addonimagesets/(production|stage)/.*.yaml",
                 changed_file,
@@ -62,12 +63,14 @@ def process_hook(api, data):
                 addon = matches.group("addon")
                 job = PRODUCTS_AND_JOBS_MAPPING.get(addon)
                 if job:
-                    trigger_openshift_ci_job(job)
+                    trigger_openshift_ci_job(
+                        job=job, addon=addon, slack_webhook_url=slack_webhook_url
+                    )
                 else:
                     app.logger.info(f"No job found for addon: {addon}")
 
 
-def trigger_openshift_ci_job(job):
+def trigger_openshift_ci_job(job, addon, slack_webhook_url):
     app.logger.info(f"Triggering openshift-ci job: {job}")
     config_data = data_from_config()
     res = requests.post(
@@ -78,9 +81,27 @@ def trigger_openshift_ci_job(job):
     res_dict = json.loads(res.text)
     if (not res.ok) or res_dict["job_status"] != "TRIGGERED":
         app.logger.error(
-            f"Failed to trigger openshift-ci job: {job}, response: {res_dict}"
+            f"Failed to trigger openshift-ci job: {job} for addon {addon}, response: {res_dict}"
         )
+
+    send_slack_message(
+        message=f"openshift-ci: New addon {addon} was merged, triggering job {job}\nresponse: {res_dict}",
+        webhook_url=slack_webhook_url,
+    )
     return res_dict
+
+
+def send_slack_message(message, webhook_url):
+    slack_data = {"text": message}
+    response = requests.post(
+        webhook_url,
+        data=json.dumps(slack_data),
+        headers={"Content-Type": "application/json"},
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            f"Request to slack returned an error {response.status_code} with the following message: {response.text}"
+        )
 
 
 @app.route("/process", methods=["POST"])
@@ -89,12 +110,13 @@ def process():
         hook_data = request.json
         event_type = hook_data["event_type"]
         repository_name = hook_data["repository"]["name"]
+        slack_webhook_url = hook_data["slack_webhook_url"]
         app.logger.info(f"{repository_name}: Event type: {event_type}")
         repository_data = repo_data_from_config(repository_name=repository_name)
         api = get_api(
             url=repository_data["gitlab_url"], token=repository_data["gitlab_token"]
         )
-        process_hook(api=api, data=hook_data)
+        process_hook(api=api, data=hook_data, slack_webhook_url=slack_webhook_url)
         return "Process done"
     except Exception as ex:
         app.logger.error(f"Failed to process hook: {ex}")
